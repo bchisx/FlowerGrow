b046fc016d4bce2a3e7973b0114ebc3ed84a00ac
Juzar Antri <juzarantri786@gmail.com>
2024-04-20 19:47:54 +0530

    addi=ed inital comment commit by juzar antri

--- FlowerGrow/src/flowergrow/Main.java
+++ FlowerGrow/src/flowergrow/Main.java
@@ -1,6 +1,7 @@
 package flowergrow;
 class Main {
 	public static void main(String args[]) {
+		/// Successfully cloned the repo - Juzar Antri
 		System.out.println("oy, my freedom is defined by growing flowers!!");
 		Petal a=new Petal(Type.DAFFODIL);
 	}
